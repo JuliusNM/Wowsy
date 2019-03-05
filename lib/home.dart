3be331514dcdca8771df5dcf4b16d26969284988
Julius <julius@Juliuss-MacBook-Pro.local>
2019-03-05 08:56:23 +0300

    Network requests

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:simple_autocomplete_formfield/simple_autocomplete_formfield.dart';
+import 'package:wowsy/requests.dart';
+
 
 class Home extends StatefulWidget {
+  final Future<MathFact> fact;
+
+  Home({Key key, this.fact}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  Future<MathFact> fact;
 
+  _HomeState({Key key, this.fact});
+  int defaultNumber = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fact = fetchFact(defaultNumber.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    Widget math = Material(
+        child: ListView(
+          children: <Widget>[
+            Container(
+                child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: NumberPicker.integer(
+                        initialValue: defaultNumber,
+                        minValue: 0,
+                        maxValue: 1000000,
+                        onChanged: (newValue) =>
+                            setState((){
+                              defaultNumber = newValue;
+                              fact = fetchFact(defaultNumber.toString());
+                            })
+                    )
+                )
+            ),
+            Card(
+              child: FutureBuilder<MathFact>(
+                future: fact,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data.body);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+            )
+
+          ],
+        )
+    );
+
     return DefaultTabController(
       length: 3,
       child: Scaffold(
@@ -24,9 +79,7 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.black,
         body: TabBarView(
           children: [
-            Container(
-              color: Colors.white,
-            ),
+            math,
             Container(
               color: Colors.white,
             ),
@@ -37,6 +90,18 @@ class _HomeState extends State<Home> {
         ),
 
       ),
+    );
+  }
+}
+class MathFact {
+  final String body;
+
+  MathFact({this.body});
+
+  factory MathFact.fromJson(Map<String, dynamic> json) {
+    print(json);
+    return MathFact(
+      body: json.toString()
     );
   }
 }
---
+++ lib/requests.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:wowsy/home.dart';
+
+Future<MathFact> fetchFact(String number) async {
+  final response =
+  await http.get('http://numbersapi.com/$number');
+
+  if (response.statusCode == 200) {
+    return MathFact.fromJson(json.decode(response.body.toString()));
+  } else {
+    throw Exception('Failed to load fact');
+  }
+}
